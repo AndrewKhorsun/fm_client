78913e03d3d35703ebf5fbed506fb90a291338ea
AndriiKh <andrii.khorsun@cruxlab.com>
2024-02-13 18:01:36 +0200

    update style

--- src/components/organism/CashFlowMonitor/CashFlowMonitorUI.tsx
+++ src/components/organism/CashFlowMonitor/CashFlowMonitorUI.tsx
@@ -43,8 +43,8 @@ export const CashFlowMonitorUI = (props: Props) => {
 				<div className='cash-flow-monitor__movement__money'>
 					<CashFlowCard title='Income:' amount={income} />
 					<CashFlowCard title='Expense:' amount={expense} />
-				</div>
 				<CashFlowCard title='Total Budget:' amount={totalBudget} />
+				</div>
 			</div>
 			<Button onClick={() => setIncomeModal(true)}>Income</Button>{' '}
 			<Button onClick={() => setExpenseModal(true)}>Expense</Button>
--- src/components/organism/Navbar/Navbar.tsx
+++ src/components/organism/Navbar/Navbar.tsx
@@ -4,6 +4,9 @@ import { Button } from '../../atoms/button/Button'
 import { logOut } from '../../../redux/auth/authSlice'
 import { useNavigateCustom } from '../../../hooks/useNavigateCustom'
 import { useDispatch } from 'react-redux'
+import { MdFamilyRestroom } from 'react-icons/md'
+import { FaPerson } from 'react-icons/fa6'
+import { RiLogoutBoxLine } from 'react-icons/ri'
 
 export const Navbar = () => {
 	const dispatch = useDispatch()
@@ -17,11 +20,18 @@ export const Navbar = () => {
 	return (
 		<nav className='navbar'>
 			<div className='navbar__links'>
-				<NavLink className='navbar__link' to='personal'>Personal Table</NavLink>
-				<NavLink className='navbar__link' to='family'>Family Table</NavLink>
+				<NavLink className='navbar__link' to='personal'>
+					<FaPerson size='25px' />
+					<p className='navbar__link--title'>Personal Table</p>
+				</NavLink>
+				<NavLink className='navbar__link' to='family'>
+					<MdFamilyRestroom size='25px' />
+					<p className='navbar__link--title'> Family Table</p>
+				</NavLink>
 			</div>
 			<Button className='navbar__button-logout' onClick={() => logOutUser()}>
-				Log Out
+				<RiLogoutBoxLine size='25px' />
+				<p className='navbar__link--title'> Log Out</p>
 			</Button>
 		</nav>
 	)
